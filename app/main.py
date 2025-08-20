507aa10b3e789849f1ba2212df9a8ca27b92b5ef
bali48 <m.bilal294@outlook.com>
2025-08-20 20:32:22 +0500

    cvc trip

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import stays, flights, cars, activities, trips, checkout, auth, packages, meta_ui, cruises, things_to_do, bookings
